867b518545cad792766d9325bb4e3a285840ef0b
M-junaid747 <muhammadjunaid0802@gmail.com>
2026-06-20 00:06:21 +0500

    Add config.py with STT/TTS settings and exit commands

---
+++ config.py
@@ -0,0 +1,14 @@
+#Speech-to-Text settings
+STT_LANG = "English" # speech language
+STT_WAIT_TIME = 5 # Time to wait before the speech starts(before giving up)
+STT_PHRASE_TIME_LIMIT = 8 # Allowed Time for the single phrase to last
+
+# Text-to-Speech settings
+TTS_SPEECH_RATE = 180 # words per minute
+TTS_VOL = 1.0 # generated speech volume
+
+# Certain name for the assistant
+ASSISTANT_NAME = "JACK"
+
+# Set of words, if spoken, should shutdown the assistant. 
+EXIT_COMMANDS = {"exit", "goodbye", "takecare", "stop", "quit", "shutdown", "see you soon"}
